648d248da3a759f2350eac3e3f90ed89314255b0
Lerena Holloway <holloway.lerena@gmail.com>
2019-02-22 16:33:22 -0500

    added semantic styling to format grid of videos

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -9,7 +9,11 @@ const VideoItem = ({ video, onVideoSelect }) => {
       }}
       className=" video-item item"
     >
-      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <img
+        className="ui image"
+        alt={video.snippet.title}
+        src={video.snippet.thumbnails.medium.url}
+      />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
       </div>
